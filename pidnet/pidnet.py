00caab3585af8bbf4bebdfaad3ade56113a83149
David Kazaryan <kazaryan.david@gmail.com>
2022-06-22 17:32:20 +0300

    Add train_step forpidnet

--- pidnet/pidnet.py
+++ pidnet/pidnet.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
 from .utils import BasicBlock, Bottleneck, SegmentHead, DAPPM, PAPPM, PagFM, Bag, LightBag
+from .losses import OHEMCrossentropy, BoundaryLoss
 
 bn_momentum = 0.1
 
@@ -96,7 +97,22 @@ class PIDNet(tf.keras.models.Model):
 
         self.final_layer = SegmentHead(head_planes, num_classes)
 
-    def call(self, inputs):
+    def train_step(self, data):
+        x, y = data
+
+        with tf.GradientTape() as tape:
+            y_pred = self(x, training=True)
+            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        self.compiled_metrics.update_state(y, y_pred)
+        return {m.name: m.result() for m in self.metrics}
+
+    def call(self, inputs, training=False):
+
         width_output = inputs.shape[2] // 8
         height_output = inputs.shape[1] // 8
 
